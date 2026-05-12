54722b8fae2109240ff30a32d3b54c6572f99f63
oajsjizizjd-gif <oajsjizizjd@gmail.com>
2026-05-12 20:54:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-import './src/app.js';
+import './src/app.js';if (message.author.bot) return;
+
+const prefix = "+";
+
+if (!message.content.startsWith(prefix)) return;
+
+const command = message.content.slice(prefix.length).trim().toLowerCase();
+
+if (command === "help") {
+  message.channel.send("📌 Commandes : +help, +ping");
+}
 
